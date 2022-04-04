4f00a637b232e8063ea8d63dbc998bfa1a590a64
ajon4 <88637514+ajon4@users.noreply.github.com>
2022-04-04 13:25:22 +0700

    Update Faq.js

--- src/components/section/Faq.js
+++ src/components/section/Faq.js
@@ -75,15 +75,11 @@ const Faq = () => {
             <Accordion title="WHAT IS THE METAVERSE?">
             A metaverse is a network of 3D virtual worlds focused on social connection. In futurism and science fiction, it is often described as a hypothetical iteration of the Internet as a single, universal virtual world that is facilitated by the use of virtual and augmented reality headsets.
             </Accordion>
-            <Accordion title="WHY DO WE NEED ROYALTIES?">
-            The amount of royalties was fixed at 5% to finance the Weirdos Club's projects. We have the ambition to organize multiple events around the world in order to strengthen the community and build a network of entrepreneurs and investors with the same mindset and common interests.
-            </Accordion>
         </Box>
         <Box>
         <Accordion title="HOW CAN I USE MY NFT?">
             You will be able to use your NFT as an avatar in the Metaverse and our future video game. Holding also means that you are part of an exclusive network of investors and entrepreneurs.
             </Accordion>
-        </Accordion>
             <Accordion title="WHY DO WE NEED ROYALTIES?">
             The amount of royalties was fixed at 5% to finance the Weirdos Club's projects. We have the ambition to organize multiple events around the world in order to strengthen the community and build a network of entrepreneurs and investors with the same mindset and common interests.
             </Accordion>
